a53c090f35741ddb85fd0ff747d8bd0d6ea79dfc
jyoti0346 <jyoti0346.be20@chitkara.edu.in>
2024-02-08 18:25:34 +0530

    changes

---
+++ terraform.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "Jahanvi" {
+  bucket = "my-tf-test-bucket3005"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+} 
+
+#--USING COUNT---
+resource "aws_s3_bucket" "Bucks" {
+  count = 3
+  
+
+  bucket = "example-bucket3005-${count.index}"
+  
+}
+
+#---USING FOR-EACH LOOP----
+
+
+resource "aws_s3_bucket" "jyoti-bucket-1" {
+
+  for_each = var.num_buckets_for_for-each
+  bucket     = each.value
+ 
+}
+
+variable "num_buckets_for_for-each" {
+  type = set(string)
+  default = ["default1", "default2", "default3", "default4"]
+}
